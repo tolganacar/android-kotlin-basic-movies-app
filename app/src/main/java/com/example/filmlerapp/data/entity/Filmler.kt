ee73f68f6c49fe15804943ded7a1c97a63d3d6ca
Tolga Nacar <83028055+tolganacar@users.noreply.github.com>
2024-03-14 14:45:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/filmlerapp/data/entity/Filmler.kt
@@ -0,0 +1,8 @@
+package com.example.filmlerapp.data.entity
+
+import java.io.Serializable
+
+data class Filmler(var id: Int,
+                   var ad: String,
+                   var resim: String,
+                   var fiyat: Int) : Serializable
